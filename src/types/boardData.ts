3cc9bacd9f29e4c6c15fa6b271b7251aef4f4b02
KirillUshakov <cool.ushackov2016@yandex.ru>
2022-10-27 21:24:47 +0300

    boardData type

---
+++ src/types/boardData.ts
@@ -0,0 +1,11 @@
+import Board from '@/classes/board/board';
+
+export type BoardData = {
+  board: Board,
+  resMultiplier: number,
+  size: {
+    width: number,
+    height: number
+  },
+  style: String[]
+}
