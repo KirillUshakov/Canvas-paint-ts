acce8e42feb7a8b1fffcc580c9498cc8e79db602
KirillUshakov <cool.ushackov2016@yandex.ru>
2022-11-03 20:25:09 +0300

    Range number input component

---
+++ src/components/paint/options/inputs/RangeInputWidthCount.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="option-item__controll-row">
+    <div class="left">
+      <input
+        v-model="curValue"
+        :min="minVal"
+        :max="maxVal"
+
+        type="range"
+        class="input-range"
+      >
+    </div>
+    <div class="right">
+      <counter v-model="curValue" :minVal="minVal" :maxVal="maxVal"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from 'vue-property-decorator'
+import Counter from '@/components/common/Counter.vue'
+
+@Component({
+  components: {
+    Counter
+  }
+})
+export default class RangeInputWidthCount extends Vue {
+  @Prop({ default: 15 }) defaultValue: number;
+  @Prop({ default: 0 }) minVal : number;
+  @Prop({ default: 1000 }) maxVal : number;
+
+  curValue = 15;
+
+  mounted () {
+    this.curValue = this.defaultValue;
+  }
+
+  @Emit('input')
+  onChange () {
+    return this.curValue;
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
